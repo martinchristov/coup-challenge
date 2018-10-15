5c28eec18fbbe363d3f11cad26a7aa89f9973b33
martin christov <martin.christov@sensika.com>
2018-10-15 13:09:51 +0300

    abort controller for fetch

--- app/api.js
+++ app/api.js
@@ -1,8 +1,10 @@
-
 export default {
 	getScooters: () => {
+		const controller = new AbortController();
+		const { signal } = controller;
+		setTimeout(() => controller.abort(), 5000);
 		return new Promise((resolve) => {
-			fetch('https://qc05n0gp78.execute-api.eu-central-1.amazonaws.com/prod/scooters')
+			fetch('https://qc05n0gp78.execute-api.eu-central-1.amazonaws.com/prod/scooters', { signal })
 				.then(d => d.json())
 				.then(resp => resolve(resp))
 		})
